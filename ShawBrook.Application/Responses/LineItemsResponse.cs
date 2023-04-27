54b96eefd103e17dc19f56be903c6a90bfd450d2
Bartlomiej Rodak <Bartlomiej.Rodak@taxsystems.com>
2023-04-27 22:44:26 +0200

    Remove not used classes

--- ShawBrook.Application/Responses/LineItemsResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShawBrook.Application.Responses
-{
-    public record LineItemResponse(
-        string Name,
-        Guid MembershipId,
-        Guid ProductId,
-        int Quantity);
-}
